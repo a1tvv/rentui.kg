5b4f320d20d58c5ff807146583b0030f54af3948
a1tvv <altaiabdurahim07@.com>
2026-02-08 10:43:25 +0600

    fiveth commit

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# Установка библиотек (включая whitenoise)
+echo "Starting Build Process..."
+
+# Используем curl для установки pip, если его нет, 
+# или просто обновляем текущий
+python3.9 -m ensurepip
+python3.9 -m pip install --upgrade pip
+
+# Устанавливаем все зависимости
 python3.9 -m pip install -r requirements.txt
 
-# Сборка статики в папку /staticfiles
+# Собираем статику
+echo "Collecting static files..."
 python3.9 manage.py collectstatic --noinput --clear
+
+echo "Build Finished!"

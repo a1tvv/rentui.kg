135459b099c991147e4f26d41f6517bb2cc706c4
a1tvv <altaiabdurahim07@gmail.com>
2026-02-08 20:03:47 +0600

    13th commit

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators

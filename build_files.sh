d64a2e966a7e5bfddb9a925c83358c2a92d44a23
a1tvv <altaiabdurahim07@.com>
2026-02-08 10:50:13 +0600

    eighth commit

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-python3 -m ensurepip
+echo "Создаем виртуальное окружение..."
+python3 -m venv venv
+source venv/bin/activate
+
+echo "Обновляем pip..."
 python3 -m pip install --upgrade pip
 
-# Ставим зависимости
+echo "Устанавливаем зависимости из requirements.txt..."
 python3 -m pip install -r requirements.txt
 
-# Собираем статику
+echo "Собираем статику..."
 python3 manage.py collectstatic --noinput --clear
+
+echo "Билд завершен!"

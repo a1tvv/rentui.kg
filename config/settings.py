9f41767bb08ec86c135df65861348df9a04f3378
a1tvv <altaiabdurahim07@gmail.com>
2026-02-13 10:02:19 +0600

    v1.05

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'django-insecure-73y4!-fk_6x$66y2n38ce9k%14n=k#rf7_h*!iq0g+y+wlx3s7
 DEBUG = False
 
 # Разрешаем все поддомены vercel и локалку
-ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

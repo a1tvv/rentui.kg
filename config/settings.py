1885c022c9c0ebbd06d570040c9a741a7aa286a4
a1tvv <altaiabdurahim07@gmail.com>
2026-02-13 09:58:28 +0600

    v1.05

--- config/settings.py
+++ config/settings.py
@@ -80,11 +80,9 @@ DATABASES = {
     }
 }
 
-
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 

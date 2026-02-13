92b16a7370d69f587565992fa079624ffea8e114
a1tvv <altaiabdurahim07@gmail.com>
2026-02-13 20:12:06 +0600

    version 1.06

--- config/settings.py
+++ config/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3', # Оставляем для локальной разработки
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
         conn_max_age=600
     )
 }

56cf0061c3f0260e249c25498b77d6ff581df4a6
a1tvv <altaiabdurahim07@gmail.com>
2026-02-16 08:43:40 +0600

    version 1.08

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 pip install -r requirements.txt
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
